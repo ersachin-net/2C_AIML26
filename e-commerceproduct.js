2bde58f0da91ccada3740edbc58768e784f2d50e
Sachin <ersachin.net@gmail.com>
2026-09-25 13:28:52 +0530

    e-commerceproduct

---
+++ e-commerceproduct.js
@@ -0,0 +1,58 @@
+class Product {
+    constructor(productId, productName, price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+    getDiscountedPrice(discount) {
+        return this.price - (this.price * discount / 100);
+    }
+    display() {
+        console.log("Product ID:", this.productId);
+        console.log("Product Name:", this.productName);
+        console.log("Price:", this.price);
+    }
+
+    static compareProducts(p1, p2) {
+        if (p1.price > p2.price) {
+            console.log(p1.productName + " has higher price.");
+        } 
+        else if (p2.price > p1.price) {
+            console.log(p2.productName + " has higher price.");
+        } 
+        else {
+            console.log("Both products have the same price.");
+        }
+    }
+}
+
+class Electronics extends Product {
+    constructor(productId, productName, price, warranty) {
+        super(productId, productName, price);
+        this.warranty = warranty;
+    }
+
+    
+    display() {
+        super.display();
+        console.log("Warranty:", this.warranty, "years");
+    }
+}
+
+
+let p1 = new Product(101, "Laptop", 60000);
+let p2 = new Product(102, "Mobile", 30000);
+
+
+console.log("Product 1:");
+p1.display();
+
+console.log("Discounted Price:", p1.getDiscountedPrice(10));
+
+
+console.log("\nComparing Products:");
+Product.compareProducts(p1, p2);
+
+console.log("\nElectronics Product:");
+let e1 = new Electronics(103, "Smart TV", 45000, 2);
+e1.display();
